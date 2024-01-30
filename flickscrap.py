6c6c14b29af8f7da521fd6c94c65bb80a233c1cc
leo <lmerkel@unistra.fr>
2024-01-30 14:51:49 +0100

    making choices for the method of scraping

--- flickscrap.py
+++ flickscrap.py
@@ -1,1 +1,15 @@
-print("hello world")
+from bs4 import BeautifulSoup
+import requests
+
+initial_url = input("rentrer l'url à la base :")
+
+initial_option = input("rentrer l'option :\n 1 : récuperer toutes les images dans le site \n 2 : récuperer les albums dans la liste \n")
+while not initial_option.isdecimal():
+    initial_option = input("valeur incorrecte")
+match int(initial_option):
+    case 1:
+        print("1")
+    case 2:
+        print("2")
+    case _:
+        raise ValueError(f"l'option est introuvable")
